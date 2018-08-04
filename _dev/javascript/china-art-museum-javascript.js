2c84a214b3ae46dc1af5d06352abe1b0343043a8
Pedro Serapio <pedro.mesopa@gmail.com>
2018-08-04 22:18:46 +0800

    Project Updated

---
+++ _dev/javascript/china-art-museum-javascript.js
@@ -0,0 +1,9 @@
+$(document).ready( function(){
+  // ==================
+  // Load Defered Fonts
+  // ==================
+  $("<link />", {
+    'rel': 'stylesheet',
+    'href': 'assets/css/china-art-museum-styles-fonts.min.css'
+  }).appendTo('head');
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 
+// -----------------
+// Project Variables
+// -----------------
+
 var gulp           = require('gulp'),
-    googleWebFonts = require('gulp-google-webfonts'),
     sass           = require('gulp-sass'),
     prefixer       = require('gulp-autoprefixer'),
     uglify         = require('gulp-uglify'),
@@ -9,224 +12,243 @@ var gulp           = require('gulp'),
     connect        = require('gulp-connect'),
     concat         = require('gulp-concat'),
     replace        = require('gulp-replace'),
-    htmlreplace    = require('gulp-html-replace'),
+    inlinesource   = require('gulp-inline-source'),
+    inject         = require('gulp-inject-string'),
     rename         = require('gulp-rename'),
+    fs             = require('fs'),
     del            = require('del');
 
-const base_path = './',
-      src       = base_path + '_dev/src',
-      dist      = base_path + 'docs';
+const theme_name   = 'china-art-museum',
+      theme_suffix = '';
 
-// Initialize the live server
-gulp.task('connect', function(){
+      const base_path  = '',
+      src        = base_path + '_dev',
+      dist       = base_path + 'docs',
+      temp       = base_path + '_dev/tmp',
+      paths      = {
+        vendor_js:   [
+          'node_modules/jquery/dist/jquery.min.js',
+          'node_modules/lazysizes/lazysizes.min.js',
+          'node_modules/lazysizes/plugins/unveilhooks/ls.unveilhooks.min.js',
+        ],
+        vendor_css:  [
+          'node_modules/basscss/css/basscss.css',
+          'node_modules/basscss-btn/css/btn.css',
+          'node_modules/basscss-btn-primary/css/btn-primary.css',
+          'node_modules/basscss-background-images/css/background-images.css',
+          'node_modules/basscss-responsive-position/css/responsive-position.css',
+          'node_modules/basscss-forms/index.css',
+        ],
+        vendor_css_AMP: [
+          'node_modules/basscss/css/basscss.css',
+          'node_modules/basscss-btn/css/btn.css',
+          'node_modules/basscss-btn-primary/css/btn-primary.css',
+          'node_modules/basscss-background-images/css/background-images.css',
+          'node_modules/basscss-responsive-position/css/responsive-position.css',
+          'node_modules/basscss-forms/index.css',
+        ],
+        js:   [
+          src + '/javascript/' + theme_name + '-javascript.js',
+        ],
+        sass: [
+          src + '/scss/' + theme_name + '-styles.scss',
+          src + '/scss/' + theme_name + '-styles-fonts.scss',
+        ],
+        sass_AMP: [
+          src + '/scss/' + theme_name + '-styles-amp.scss',
+          src + '/scss/' + theme_name + '-styles-fonts-amp.scss',
+        ]
+      };
+
+// -------------
+// Project Tasks
+// -------------
+
+// Local Server
+gulp.task('connect', () => {
   connect.server({
     root: dist,
     livereload: false
   });
 });
 
-// Copy and minify HTML files
-gulp.task('html', function(){
-  return gulp.src( src + '/*.html' )
-    .pipe(htmlmin({
-      collapseWhitespace: true,
-      collapseInlineTagWhitespace: true
-    }))
-    .pipe(gulp.dest( dist ))
-    .pipe(connect.reload());
-});
-
-// Copy all necessary images
-gulp.task('images', function(){
-  return gulp.src( src + '/images/**/*' )
-    .pipe(gulp.dest( dist + '/assets/images/' ))
-    .pipe(connect.reload());
-});
-
-// Get all the necessary Google fonts listed in 'fonts.list'
-gulp.task('google-fonts', function(){
-  return gulp.src( base_path + 'fonts.list' )
-    .pipe(googleWebFonts({
-      fontsDir: dist + '/assets/fonts/google/',
-      cssDir: dist + '/assets/css/',
-      cssFilename: 'fonts.css'
-    }))
-    .pipe(gulp.dest('./'));
-});
-
-// Fix CSS Google Fonts PATH
-gulp.task('google-fonts-css-fix', ['google-fonts'], function(){
-  return gulp.src( dist + '/assets/css/fonts.css')
-    .pipe(replace('url(docs/assets/', 'url(../'))
-    .pipe(minifyCSS())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest(dist + '/assets/css/'));
-});
-
-// Copy scripts that doesn't need any compilation
-gulp.task('no-compile-scripts', function(){
-  return gulp.src([
-    //'node_modules/jquery/dist/jquery.min.js', //Only for local development
-    'node_modules/lazysizes/lazysizes.min.js',
-    src + '/javascript/ls.unveilhooks.min.js'
-  ])
-    .pipe(gulp.dest( dist + '/assets/js/' ));
-});
-
-// Compile Javascript scripts.js
-gulp.task('compile-scripts', function(){
-  return gulp.src( src + '/javascript/scripts.js')
-    .pipe(uglify())
-    .pipe(rename('scripts.min.js'))
-    .pipe(gulp.dest( dist + '/assets/js/' ));
-});
-
-
-// Compile styles.scss
-gulp.task('styles-sass', function(){
-  return gulp.src( src + '/stylesheet/styles.scss' )
-    .pipe(sass())
-    .pipe(minifyCSS())
-    .pipe(prefixer())
-    .pipe(rename('styles.min.css'))
-    .pipe(gulp.dest( dist + '/assets/css/' ));
-});
-
-
-// Concat all Styles Files into one file
-gulp.task('concat-styles', [
-  'google-fonts',
-  'google-fonts-css-fix',
-  'styles-sass' ], function(){
-  return gulp.src([
-   './node_modules/basscss/css/basscss.css',
-   './node_modules/basscss-btn/css/btn.css',
-   './node_modules/basscss-btn-primary/css/btn-primary.css',
-   './node_modules/basscss-background-images/css/background-images.css',
-   './node_modules/basscss-responsive-position/css/responsive-position.css',
-   './node_modules/basscss-forms/index.css',
-   dist + '/assets/css/fonts.min.css',
-   dist + '/assets/css/styles.min.css'
-  ])
-  .pipe(concat('app.min.css'))
-  .pipe(minifyCSS({
-    level: {1: {specialComments: 0}}
-  }))
-  .pipe(gulp.dest( dist + '/assets/css/' ));
-});
-
-// Concat all Scripts Files into one file
-gulp.task('concat-scripts', [
-  'no-compile-scripts',
-  'compile-scripts' ], function(){
-return gulp.src([
-    dist + '/assets/js/lazysizes.min.js',
-    dist + '/assets/js/ls.unveilhooks.min.js',
-    dist + '/assets/js/scripts.min.js'
-  ])
-  .pipe(concat('app.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest( dist + '/assets/js/' ));
-});
-
-// Live concat all styles files into one file
-// If any changes are made in styles.scss
-gulp.task('watch-concat-styles', ['styles-sass'], function(){
-  return gulp.src([
-    './node_modules/basscss/css/basscss.css',
-    './node_modules/basscss/css/basscss.css',
-    './node_modules/basscss-btn/css/btn.css',
-    './node_modules/basscss-btn-primary/css/btn-primary.css',
-    './node_modules/basscss-background-images/css/background-images.css',
-    './node_modules/basscss-responsive-position/css/responsive-position.css',
-    './node_modules/basscss-forms/index.css',
-    dist + '/assets/css/fonts.min.css',
-    dist + '/assets/css/styles.min.css'
-   ])
-   .pipe(concat('app.min.css'))
-   .pipe(minifyCSS({
-     level: {1: {specialComments: 0}}
-   }))
-   .pipe(gulp.dest( dist + '/assets/css/' ))
-   .pipe(connect.reload());
-});
-
-// Live concat all scripts files into one file
-// If any changes are made in scripts.js
-gulp.task('watch-concat-scripts', ['compile-scripts'], function(){
-  return gulp.src([
-    dist + '/assets/js/lazysizes.min.js',
-    dist + '/assets/js/ls.unveilhooks.min.js',
-    dist + '/assets/js/scripts.min.js'
-  ])
-  .pipe(concat('app.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest( dist + '/assets/js/' ))
-  .pipe(connect.reload());
-});
-
-// Clean all files inside the 'dist' folder
-gulp.task('clean', function(){
+// -- CLEAN 'dist' --
+gulp.task('clean-dist', () => {
   return del( dist + '/*' );
 });
 
-// Watch styles.scss and scripts.js for any changes
-gulp.task('watch', function(){
-  gulp.watch( src + '/*.html', ['html'] );
-  gulp.watch( src + '/stylesheet/*.scss', ['watch-concat-styles'] );
-  gulp.watch( src + '/javascript/scripts.js', ['watch-concat-scripts'] );
+// -- CLEAN 'tmp' --
+gulp.task('clean-tmp', () => {
+  return del( temp + '/*' );
 });
 
-// *****************************************
-// AMP Section
-// *****************************************
+// -- ASSETS --
+gulp.task('assets', () => {
+  return gulp.src( src + '/assets/**/**/**/*' )
+    .pipe( gulp.dest( dist + '/assets/' ) );
+});
 
-gulp.task('amp-styles-sass', function(){
-  return gulp.src( src + '/amp/stylesheet/amp-styles.scss' )
-    .pipe(sass())
-    .pipe(minifyCSS())
-    .pipe(rename({
+// -- JS     --
+gulp.task('js', () => {
+  return gulp.src( paths.js )
+    .pipe( concat( theme_name + theme_suffix + '.js' ) )
+    .pipe( uglify())
+    .pipe( rename({
       suffix: '.min'
     }))
-    .pipe(gulp.dest( dist + '/assets/css/' ));
+    .pipe( gulp.dest( temp + '/compiled-js' ) );
 });
 
-gulp.task('amp-inline-styles', ['amp-styles-sass'], function(){
-  return gulp.src( src + '/amp/index.amp.html' )
-    .pipe(htmlreplace({
-      'cssInline': {
-        'src': gulp.src( dist + '/assets/css/amp-styles.min.css' ),
-        'tpl': '<style amp-custom>%s</style>'
-      }
+// -- SASS   --
+gulp.task('sass', () => {
+  return gulp.src( paths.sass )
+    .pipe( sass())
+    .pipe( prefixer({
+      browsers: [
+        'last 2 versions',
+        '> 1%',
+        'opera 12.1',
+        'bb 10',
+        'android 4'
+        ]
+      }))
+    .pipe( minifyCSS({
+        level: {1: {specialComments: 0}}
+      }))
+    .pipe( rename({
+      suffix: '.min'
     }))
-    .pipe(gulp.dest( dist + '/' ));
+    .pipe( gulp.dest( temp + '/compiled-css' ) )
+    .pipe( gulp.dest( dist + '/assets/css' ) );
 });
 
-gulp.task('amp-watch', function(){
-  gulp.watch( src + '/amp/stylesheet/amp-styles.scss', ['amp-inline-styles'] );
-  gulp.watch( src + '/amp/index.amp.html', ['amp-inline-styles'] );
+// -- SASS AMP  --
+gulp.task('sass-amp', () => {
+  return gulp.src( paths.sass_AMP )
+    .pipe( sass())
+    .pipe( prefixer({
+      browsers: [
+        'last 2 versions',
+        '> 1%',
+        'opera 12.1',
+        'bb 10',
+        'android 4'
+        ]
+      }))
+    .pipe( replace( '!important', '' ) )
+    .pipe( minifyCSS({
+        level: {1: {specialComments: 0}}
+      }))
+    .pipe( rename({
+      suffix: '.min'
+    }))
+    .pipe( gulp.dest( temp + '/compiled-css' ) );
 });
 
-gulp.task('amp', function(){
-  gulp.start(
-    'amp-inline-styles',
-    'connect',
-    'amp-watch');
+// -- Vendor JS    --
+gulp.task('vendor-js', () => {
+  return gulp.src( paths.vendor_js )
+    .pipe( concat( theme_name + theme_suffix + '-vendor' + '.js' ) )
+    .pipe( uglify())
+    .pipe( rename({
+      suffix: '.min'
+    }))
+    .pipe( gulp.dest( temp + '/compiled-js' ) );
 });
 
-// *****************************************
-// End of AMP Section
-// *****************************************
-
-// Default task
-gulp.task('default', ['clean'], function(){
-  gulp.start(
-    'html',
-    'images',
-    'concat-styles',
-    'concat-scripts',
-    'connect',
-    'watch');
+// -- Vendor CSS   --
+gulp.task('vendor-css', () => {
+  return gulp.src( paths.vendor_css )
+    .pipe( concat( theme_name + theme_suffix + '-vendor' + '.css' ) )
+    .pipe( minifyCSS({
+      level: {1: {specialComments: 0}}
+    }))
+    .pipe( rename({
+      suffix: '.min'
+    }))
+    .pipe( gulp.dest( temp + '/compiled-css' ) );
 });
+
+// -- Vendor CSS AMP  --
+gulp.task('vendor-css-amp', () => {
+  return gulp.src( paths.vendor_css_AMP )
+    .pipe( concat( theme_name + theme_suffix + '-vendor' + '.css' ) )
+    .pipe( replace( '!important', '' ) )
+    .pipe( minifyCSS({
+      level: {1: {specialComments: 0}}
+    }))
+    .pipe( rename({
+      suffix: '-amp.min'
+    }))
+    .pipe( gulp.dest( temp + '/compiled-css' ) );
+});
+
+// -- Inline CSS   --
+gulp.task('inline-css', ['sass', 'vendor-css'], ()=> {
+  return gulp.src( src + '/html/*' )
+     .pipe( inlinesource() )
+     .pipe( htmlmin({
+       collapseWhitespace: true
+      }))
+     .pipe( gulp.dest( dist ) )
+     .pipe( connect.reload() );
+});
+
+// -- Concat CSS AMP --
+gulp.task('concat-css-amp', ['sass-amp', 'vendor-css-amp'], ()=> {
+  return gulp.src([
+       temp + '/compiled-css/china-art-museum-vendor-amp.min.css',
+       temp + '/compiled-css/china-art-museum-styles-amp.min.css',
+       temp + '/compiled-css/china-art-museum-styles-fonts-amp.min.css'
+     ])
+     .pipe( concat(theme_name + '-amp-all.min.css'))
+     .pipe( gulp.dest( temp + '/compiled-css' ));
+});
+
+// -- Inline CSS AMP  --
+gulp.task('inline-css-amp', ['concat-css-amp'], ()=> {
+  var cssContent = fs.readFileSync( temp + '/compiled-css/china-art-museum-amp-all.min.css');
+
+  return gulp.src( src + '/html/amp/*' )
+     .pipe( inject.after('style amp-custom>', cssContent) )
+     .pipe( htmlmin({
+       collapseWhitespace: true
+      }))
+     .pipe( gulp.dest( dist ) )
+     .pipe( connect.reload() );
+});
+
+// -- Concat JS   --
+gulp.task('concat-js', ['js', 'vendor-js'], () => {
+  return gulp.src([
+    temp + '/compiled-js/' + theme_name + '-vendor.min.js',
+    temp + '/compiled-js/' + theme_name + '.min.js',
+    temp + '/compiled-js/cloudimage.io-responsive.js'
+  ])
+  .pipe( concat(theme_name + '-all.min.js') )
+  .pipe( gulp.dest( dist + '/assets/js' ) )
+  .pipe( connect.reload() );
+});
+
+// -- WATCH          --
+gulp.task('watch', () => {
+  gulp.watch( src + '/html/*', ['inline-css', 'inline-css-amp'] );
+  gulp.watch( src + '/javascript/*', ['concat-js'] );
+  gulp.watch( src + '/scss/*', ['inline-css', 'inline-css-amp'] );
+});
+
+// ------------------------------
+// -- Project `default` Gulp Task
+// ------------------------------
+
+gulp.task(
+  'default', ['clean-dist', 'clean-tmp'], () => {
+    gulp.start(
+      'connect',
+      'assets',
+      'concat-js',
+      'inline-css',
+      'inline-css-amp',
+      'watch'
+    )
+  });
